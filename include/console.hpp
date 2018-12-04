c65eebe3946d6a16d1e5aeb16c0b53bec0374c33
Damian Tran <damian.tran@aifive.tech>
2018-12-03 20:19:57 -0500

    Updated header files

--- include/console.hpp
+++ include/console.hpp
@@ -1,3 +1,18 @@
+//
+// CVision: a multi-platform graphics interface libary for C++
+//
+//////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2017 - 2018 Damian Tran
+//
+// CVision makes use of SFML (Simple and Fast Multimedia Library)
+// Copyright (c) Laurent Gomila
+// See licence: www.sfml-dev.org/license.php
+//
+// Redistribution of CVision is permitted under accordance with
+// the GNU general public licence (GPL) version 3.0 and the
+// terms and conditions specified in CVlicence.txt
+
 #pragma once
 
 #ifndef CVIS_CONSOLE
--- include/panel.hpp
+++ include/panel.hpp
@@ -1,29 +1,17 @@
-/* =========================================================================
- ===========================================================================
- ===========================================================================
-
-		     AiDA - The Artificial Intelligence Desktop Assistant
-
-	         Copyright (C) 2018 Damian Tran.  All rights reserved.
-
-                          - See AiDA-licence.txt -
-
- ===========================================================================
- ===========================================================================
- ===========================================================================
-
- AiDA incorporates technologies held under the following copyrights:
-
- OpenSSL - Copyright (c) 1998-2018 The OpenSSL Project
-
- The Boost C++ Library:
-
-          Copyright Joe Coder 2004 - 2006.
- Distributed under the Boost Software License, Version 1.0.
-      (See https://www.boost.org/LICENSE_1_0.txt)
-
- CVision - Copyright (c) 2017 - 2018 Damian Tran (see CVlicence.txt)
-                                                                            */
+//
+// CVision: a multi-platform graphics interface libary for C++
+//
+//////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2017 - 2018 Damian Tran
+//
+// CVision makes use of SFML (Simple and Fast Multimedia Library)
+// Copyright (c) Laurent Gomila
+// See licence: www.sfml-dev.org/license.php
+//
+// Redistribution of CVision is permitted under accordance with
+// the GNU general public licence (GPL) version 3.0 and the
+// terms and conditions specified in CVlicence.txt
 
 #pragma once
 
